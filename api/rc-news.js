3803c2b66b0e448d60cf909e1a5fef0d91136f71
Loociez <loocielambert@gmail.com>
2026-03-08 15:02:42 +0000

    up

--- api/rc-news.js
+++ api/rc-news.js
@@ -1,37 +1,40 @@
-// api/rc-news.js
 export default async function handler(req, res) {
   try {
     const rssUrl = 'https://www.bigsquidrc.com/feed';
     const response = await fetch(rssUrl);
     const xmlText = await response.text();
 
-    // Minimal parsing: extract first 5 <item> blocks
     const items = [];
     const itemRegex = /<item>([\s\S]*?)<\/item>/g;
     let match;
-    while ((match = itemRegex.exec(xmlText)) && items.length < 5) {
-      const itemText = match[1];
 
-      const getTag = (tag) => {
-        const m = new RegExp(`<${tag}>([\\s\\S]*?)<\\/${tag}>`).exec(itemText);
-        return m ? m[1] : '';
+    while ((match = itemRegex.exec(xmlText)) && items.length < 5) {
+      const item = match[1];
+
+      const get = (tag) => {
+        const m = new RegExp(`<${tag}>([\\s\\S]*?)<\\/${tag}>`).exec(item);
+        return m ? m[1] : "";
       };
 
       items.push({
-        title: getTag('title'),
-        link: getTag('link'),
-        description: getTag('description'),
-        pubDate: getTag('pubDate'),
+        title: get("title"),
+        link: get("link"),
+        description: get("description"),
+        pubDate: get("pubDate")
       });
     }
 
-    // CORS headers so GitHub Pages can fetch it
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Content-Type', 'application/json');
+    // IMPORTANT: CORS headers
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
     res.status(200).json({ items });
 
   } catch (err) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(500).json({ error: err.message });
+
   }
 }
